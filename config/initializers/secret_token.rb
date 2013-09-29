ae021e07ca2d60c68516fc3fe27910fef7d90f74
Derik Strattan <derik.strattan@gmail.com>
2013-09-28 20:08:24 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KFreezePop::Application.config.secret_key_base = '28aca3fcf89249497cfb90074f0feae530717261fc47fea0243d1abe67708f05ecd2f2e6456cf84199afb0146feb1a91621cca0d18ce312d521c991d51e5b8fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KFreezePop::Application.config.session_store :cookie_store, key: '_k_freeze_pop_session'
