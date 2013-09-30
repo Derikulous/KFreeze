4eee381feab4d4a68e83a1a0d6fe7554b2cdc440
Derik Strattan <derik.strattan@gmail.com>
2013-09-29 23:33:34 -0700

    Finished adding user configuration and actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ KFreezePop::Application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  match '/edit',  to: 'users#edit',            via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,9 +15,22 @@ describe User do
  it { should respond_to(:password) }
  it { should respond_to(:password_confirmation) }
  it { should respond_to(:authenticate) }
- it { should be_valid }
+ it { should respond_to(:admin) }
 
- describe "when name is not present" do
+ it { should be_valid }
+ it { should_not be_admin }
+
+
+ describe "with admin attribute set to 'true'" do
+  before do
+    @user.save!
+    @user.toggle!(:admin)
+  end
+
+  it { should be_admin }
+end
+
+describe "when name is not present" do
   before { @user.user_name = " " }
   it { should_not be_valid }
 end
@@ -92,13 +105,13 @@ describe "with a password that's too short" do
   it { should be_invalid }
 end
 
-  describe "email address with mixed case" do
-    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+describe "email address with mixed case" do
+  let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
-    it "should be saved as all lower-case" do
-      @user.email = mixed_case_email
-      @user.save
-      expect(@user.reload.email).to eq mixed_case_email.downcase
-    end
+  it "should be saved as all lower-case" do
+    @user.email = mixed_case_email
+    @user.save
+    expect(@user.reload.email).to eq mixed_case_email.downcase
   end
+end
 end
