9457229febcf3817b40947b8710581a1079137f1
Derik Strattan <derik.strattan@gmail.com>
2014-01-28 22:18:06 -0800

    added 2am video links

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ KFreezePop::Application.routes.draw do
 
   resources :users , only: [:show, :edit, :update, :index]
 
-  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/2am',    to: 'static_pages#2am',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
